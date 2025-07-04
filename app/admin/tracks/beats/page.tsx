dac28377e1eaec485c20bf3c89effe67c922d1af
ManoharaKKK <kavindamanoharak@gmail.com>
2025-07-04 17:38:00 +0530

    done

--- app/admin/tracks/beats/page.tsx
+++ app/admin/tracks/beats/page.tsx
@@ -1,25 +1,9 @@
 "use client";
 import React, { useState } from "react";
 import { FaEye, FaEdit, FaTrash, FaTimes } from "react-icons/fa";
+import { BeatCategory, initialBeats } from "./beatData";
 
-// Temporary local data type and initial data
-export interface BeatCategory {
-  id: number;
-  name: string;
-  description: string;
-}
 
-export const initialBeats: BeatCategory[] = [
-  { id: 1, name: "Electronics01", description: "Lorem ipsum is simply dummy...." },
-  { id: 2, name: "Electronics02", description: "Lorem ipsum is simply dummy...." },
-  { id: 3, name: "Electronics03", description: "Lorem ipsum is simply dummy...." },
-  { id: 4, name: "Electronics04", description: "Lorem ipsum is simply dummy...." },
-  { id: 5, name: "Electronics05", description: "Lorem ipsum is simply dummy...." },
-  { id: 6, name: "Electronics06", description: "Lorem ipsum is simply dummy...." },
-  { id: 7, name: "Electronics07", description: "Lorem ipsum is simply dummy...." },
-  { id: 8, name: "Electronics08", description: "Lorem ipsum is simply du...." },
-  { id: 9, name: "Electronics09", description: "Lorem ipsum is simply dum...." },
-];
 
 export default function BeatCategoryPage() {
   const [beats, setBeats] = useState<BeatCategory[]>(initialBeats);
